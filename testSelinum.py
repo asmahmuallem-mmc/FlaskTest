adb9b6a8754acf1e737ed72d9333595d78467a5c
asmahmuallem-mmc <asmah.muallem@mmc.edu>
2025-03-19 18:13:42 -0500

    adding testSelinum to feature branch

---
+++ testSelinum.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.python.org")
+
+print(driver.title)
+print(driver.current_url)
+print(driver.page_source)
